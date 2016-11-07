67fe6254f1e6229202cf1240d20174573568d340
Wilson Campusano <wicampusano@arshumano.com>
2016-11-07 09:27:53 -0400

    configuration.

---
+++ src/main/java/com/gmail/wilsoncampusanojorge/SagloApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.wilsoncampusanojorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagloApplication.class, args);
+	}
+}
