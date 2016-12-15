dd5b5ed63a01cfffdc4001b3fdc0b90ec542b7b1
Wilson Campusano <wicampusano@arshumano.com>
2016-12-15 14:14:07 -0400

    spring security  controller log in and log out

---
+++ src/main/java/com/gmail/wilsoncampusanojorge/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.gmail.wilsoncampusanojorge.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = {"/login", "/"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView("login");
+        return modelAndView;
+    }
+
+
+    public void logout(){
+
+    }
+}
