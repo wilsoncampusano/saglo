da3b8b95da3d1f20c440d79db3b09680ef42308a
Wilson Campusano <wicampusano@arshumano.com>
2016-12-19 08:01:11 -0400

    remove comments .

--- src/main/java/com/gmail/wilsoncampusanojorge/entities/dominio/Local.java
+++ src/main/java/com/gmail/wilsoncampusanojorge/entities/dominio/Local.java
@@ -1,7 +1,5 @@
 package com.gmail.wilsoncampusanojorge.entities.dominio;
 
-/**
- * Created by wicampusano on 12/19/2016.
- */
+
 public class Local {
 }
--- src/main/java/com/gmail/wilsoncampusanojorge/entities/seguridad/Role.java
+++ src/main/java/com/gmail/wilsoncampusanojorge/entities/seguridad/Role.java
@@ -1,7 +1,5 @@
 package com.gmail.wilsoncampusanojorge.entities.seguridad;
 
-/**
- * Created by wicampusano on 12/19/2016.
- */
+
 public class Role {
 }
