f7e4d7e0754a57729572af5e8edeba5da0c1b17a
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-12-22 13:00:27 -0400

    controller refactor

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/AutorizacionAutenticacionController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/AutorizacionAutenticacionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AutorizacionAutenticacionController {
 
-    @RequestMapping(value = {"/login", "/"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/login", "/Login"}, method = RequestMethod.GET)
     public ModelAndView login(){
         ModelAndView modelAndView = new ModelAndView("login");
         return modelAndView;
--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/HomeController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-  @RequestMapping(path = {"/", "/index", "/home"}, method = RequestMethod.GET)
+  @RequestMapping(path = {"/index", "/home"}, method = RequestMethod.GET)
   public ModelAndView index(){
     ModelAndView modelAndView = new ModelAndView("landing/index");
     return modelAndView;
