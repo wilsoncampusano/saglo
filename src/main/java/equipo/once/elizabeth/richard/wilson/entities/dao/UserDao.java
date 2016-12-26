0a5afbbfbd0354bd88364f379a05403c3a4b997c
Wilson Campusano <wicampusano@arshumano.com>
2016-12-26 08:11:40 -0400

    remove database configuration.

--- src/main/java/equipo/once/elizabeth/richard/wilson/entities/dao/UserDao.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/entities/dao/UserDao.java
@@ -1,9 +1,6 @@
 package equipo.once.elizabeth.richard.wilson.entities.dao;
 
 import equipo.once.elizabeth.richard.wilson.entities.seguridad.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
-public interface UserDao extends CrudRepository<User, Long> {
+public interface UserDao  {
 }
--- src/main/java/equipo/once/elizabeth/richard/wilson/entities/seguridad/User.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/entities/seguridad/User.java
@@ -2,12 +2,9 @@ package equipo.once.elizabeth.richard.wilson.entities.seguridad;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "usuario")
+
 public class User {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String username;
   private String password;
