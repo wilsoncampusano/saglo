906ea158230abbb924ff1d0f68615a7baee0665b
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-12-31 11:14:40 -0400

    test api area comun.

--- src/main/java/equipo/once/elizabeth/richard/wilson/usecases/BuscarAreaComunUseCase.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/usecases/BuscarAreaComunUseCase.java
@@ -6,12 +6,14 @@ import equipo.once.elizabeth.richard.wilson.usecases.dtos.BuscarAreaComunRequest
 import equipo.once.elizabeth.richard.wilson.usecases.dtos.BuscarAreaComunResponse;
 import equipo.once.elizabeth.richard.wilson.usecases.interfaces.AreaComunResponse;
 import equipo.once.elizabeth.richard.wilson.usecases.interfaces.AreaComunUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BuscarAreaComunUseCase implements AreaComunUseCase {
+  @Autowired
   public AreaComunService areaComunService;
   public BuscarAreaComunRequest request;
   private BuscarAreaComunResponse response;
