36197c572a75d6af80a27592e53639790a4572a4
Wilson Campusano <wicampusano@arshumano.com>
2017-01-02 08:06:56 -0400

    build areacomun autocomplete

--- src/main/resources/static/js/solicitudarea.js
+++ src/main/resources/static/js/solicitudarea.js
@@ -2,11 +2,30 @@
  * Created by wilsoncampusano on 12/31/16.
  */
 $(document).ready(function(){
-    $('input.autocomplete').autocomplete({
-        data: {
-            "Apple": null,
-            "Microsoft": null,
-            "Google": 'http://placehold.it/250x250'
+    $('#autocomplete-areacomun').autocomplete({
+        source: function (request, response) {
+            var criterio = this.term;
+
+            $.ajax("/api/areacomun/buscar", {
+                global: false,
+                success: function (areaComunResponse) {
+                    response($.map(areaComunResponse, function (elem, idx) {
+                        return {
+                            label: elem.nombre,
+                            name: elem.nombre,
+                            code: elem.codigo
+                        }
+                    }));
+                }
+            });
+        },
+        response: function (event, ui) {
+            if (ui.content.length <= 0) {
+                ui.content.push({label: "No se encontraron resultados"});
+            }
+        },
+        select: function (event, ui) {
+
         }
     });
 
