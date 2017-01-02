f2c4ea93d531ce008a3ecf4a65b6a42b4958b45d
Wilson Campusano <wicampusano@arshumano.com>
2017-01-02 07:40:52 -0400

    js

---
+++ src/main/resources/static/js/areacomunautocomplete.js
@@ -0,0 +1,11 @@
+
+$.fn.areacomunautocomplete = function (settings) {
+    this.autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'http://placehold.it/250x250'
+        }
+    });
+};
+
---
+++ src/main/resources/static/js/solicitud/registrar/areacomun.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wicampusano on 12/28/2016.
+ */
+$(document).ready(function(){
+
+$('#autocomplete-areacomun').areacomunautocomplete({});
+
+});
