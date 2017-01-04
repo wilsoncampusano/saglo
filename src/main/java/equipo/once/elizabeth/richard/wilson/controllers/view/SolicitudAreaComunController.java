cb9d8022a2dacaf8ed912255563d82df38772a1b
Wilson Campusano <wicampusano@arshumano.com>
2017-01-04 08:11:18 -0400

    solicitud area controller.

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/SolicitudAreaComunController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/SolicitudAreaComunController.java
@@ -1,17 +1,39 @@
 package equipo.once.elizabeth.richard.wilson.controllers.view;
 
+import equipo.once.elizabeth.richard.wilson.usecases.dtos.SolicitudAreaComunForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class SolicitudAreaComunController {
 
 
+    public static final String SOLICITUDES_AREACOMUN_REGISTRAR = "/solicitudes/areacomun/registrar";
+
     @RequestMapping(value = "/solicitudes/areacomun", method = RequestMethod.GET)
     public ModelAndView solicitudesAreaComunGet(){
-        ModelAndView modelAndView = new ModelAndView("/solicitudes/areacomun/registrar");
+        ModelAndView modelAndView = new ModelAndView(SOLICITUDES_AREACOMUN_REGISTRAR);
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/solicitudes/areacomun",params = {"registrar"}, method = RequestMethod.POST)
+    public ModelAndView solicitudesAreaComunPostRegistrar(SolicitudAreaComunForm form , RedirectAttributes redirectAttrs,
+                                                 HttpSession httpSession){
+        ModelAndView modelAndView = new ModelAndView(SOLICITUDES_AREACOMUN_REGISTRAR);
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/solicitudes/areacomun",params = {"cancelar"}, method = RequestMethod.POST)
+    public ModelAndView solicitudesAreaComunPostCancelar(SolicitudAreaComunForm form , RedirectAttributes redirectAttrs,
+                                                 HttpSession httpSession){
+        ModelAndView modelAndView = new ModelAndView(SOLICITUDES_AREACOMUN_REGISTRAR);
 
         return modelAndView;
     }
