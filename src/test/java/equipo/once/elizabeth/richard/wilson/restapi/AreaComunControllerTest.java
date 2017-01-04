0a1e4c150990e74a891c597ccf20b5c1a2872e58
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-01-03 20:22:06 -0400

    clean

--- src/test/java/equipo/once/elizabeth/richard/wilson/restapi/AreaComunControllerTest.java
+++ src/test/java/equipo/once/elizabeth/richard/wilson/restapi/AreaComunControllerTest.java
@@ -1,33 +1,22 @@
 package equipo.once.elizabeth.richard.wilson.restapi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import equipo.once.elizabeth.richard.wilson.SagloApplication;
-import equipo.once.elizabeth.richard.wilson.usecases.interfaces.AreaComunResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.List;
-
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.notNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SagloApplication.class)
