ce52a52ec197695d84bfc39209e2b8fc11e8bc76
Wilson Campusano <wicampusano@arshumano.com>
2017-01-05 08:09:57 -0400

    estructura controllers.

---
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/inquilino/InquilinoPerfilController.java
@@ -0,0 +1,10 @@
+package equipo.once.elizabeth.richard.wilson.controllers.view.inquilino;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by wicampusano on 1/5/2017.
+ */
+@Controller
+public class InquilinoPerfilController extends InquilinoController {
+}
