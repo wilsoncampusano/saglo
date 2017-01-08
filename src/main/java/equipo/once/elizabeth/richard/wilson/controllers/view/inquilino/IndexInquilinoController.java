0ae2144a295784b501f544dc37e0f9613fc7d956
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-01-08 09:26:59 -0400

    inquilino index .

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/inquilino/IndexInquilinoController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/inquilino/IndexInquilinoController.java
@@ -1,6 +1,5 @@
 package equipo.once.elizabeth.richard.wilson.controllers.view.inquilino;
 
-import equipo.once.elizabeth.richard.wilson.controllers.view.CommonViewController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/inquilino")
 public class IndexInquilinoController extends InquilinoController {
 
-    public static final String SOLICITUDES_AREACOMUN_REGISTRAR = "/inquilino/index";
+    public static final String INQUILINO_INDEX = "/inquilino/index";
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView solicitudesAreaComunGet(){
-        ModelAndView modelAndView = new ModelAndView(SOLICITUDES_AREACOMUN_REGISTRAR);
+    @RequestMapping(value = {"/","","/index", "home"}, method = RequestMethod.GET)
+    public ModelAndView inquilinoIndexGet(){
+        ModelAndView modelAndView = new ModelAndView(INQUILINO_INDEX);
 
         return modelAndView;
     }
