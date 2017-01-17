a392a6a426672ee674ec8033908ddb889e38565e
Wilson Campusano <wicampusano@arshumano.com>
2017-01-17 11:32:32 -0400

    solicitud solucion averia inquilino.

---
+++ src/test/java/equipo/once/elizabeth/richard/wilson/usecases/inquilino/SolicitudSolucionAveriaTest.java
@@ -0,0 +1,13 @@
+package equipo.once.elizabeth.richard.wilson.usecases.inquilino;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolicitudSolucionAveriaTest {
+
+    @Test
+    public void inquilinoSolicitaSolucionAveria() {
+        Assert.fail();
+    }
+}
