dcc3ec26b6cda8684483359e64b6c89b8641b1d0
Wilson Campusano <wicampusano@arshumano.com>
2017-01-18 15:35:37 -0400

    areacomun repository implementacion.

--- src/main/java/equipo/once/elizabeth/richard/wilson/entities/dao/impl/AreaComunRepositoryImpl.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/entities/dao/impl/AreaComunRepositoryImpl.java
@@ -2,20 +2,10 @@ package equipo.once.elizabeth.richard.wilson.entities.dao.impl;
 
 import equipo.once.elizabeth.richard.wilson.entities.dao.AreaComunRepositoryCustom;
 import equipo.once.elizabeth.richard.wilson.entities.dominio.AreaComun;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class AreaComunRepositoryImpl implements AreaComunRepositoryCustom{
-
-  AreaComun piscina = AreaComun.crear("A-piscina-1","Piscina");
-  AreaComun areaBBQ = AreaComun.crear("A-BBQ-1", "BBQ");
-  List<AreaComun> areasComunes = Arrays.asList(piscina, areaBBQ);
-
 
   @Override
   public List<AreaComun> findByNombreIgnoreCaseContaining(String palabra) {
@@ -29,42 +19,42 @@ public class AreaComunRepositoryImpl implements AreaComunRepositoryCustom{
 
   @Override
   public <S extends AreaComun> Iterable<S> save(Iterable<S> entities) {
-    return null;
+    return this.save(entities);
   }
 
   @Override
   public AreaComun findOne(Long aLong) {
-    return null;
+    return this.findOne(aLong);
   }
 
   @Override
   public boolean exists(Long aLong) {
-    return false;
+    return this.exists(aLong);
   }
 
   @Override
   public Iterable<AreaComun> findAll() {
-    return null;
+    return this.findAll();
   }
 
   @Override
   public Iterable<AreaComun> findAll(Iterable<Long> longs) {
-    return null;
+    return this.findAll(longs);
   }
 
   @Override
   public long count() {
-    return 0;
+    return this.count();
   }
 
   @Override
   public void delete(Long aLong) {
-
+    this.delete(aLong);
   }
 
   @Override
   public void delete(AreaComun entity) {
-
+    this.delete(entity);
   }
 
   @Override
--- src/main/java/equipo/once/elizabeth/richard/wilson/services/UserService.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/services/UserService.java
@@ -1,7 +1,6 @@
 package equipo.once.elizabeth.richard.wilson.services;
 
 import equipo.once.elizabeth.richard.wilson.entities.dao.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
