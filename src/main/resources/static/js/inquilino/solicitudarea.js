08ce12bc140efbc13d8f27ed382c67d22fc43550
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-04-02 10:14:52 -0400

    iief

--- src/main/resources/static/js/inquilino/solicitudarea.js
+++ src/main/resources/static/js/inquilino/solicitudarea.js
@@ -1,55 +1,62 @@
+(function (solicitudAreaCode) {
 
-$(document).ready(function(){
-    $('#menu_inquilino_areacomun').addClass("active");
-    configurarAutoCompleteAreaComun();
-    configurarFechaSolicitud();
-});
+    solicitudAreaCode(window.jQuery, window, document);
 
+}(function ($, window, document) {
+        $(function () {
+            $('#menu_inquilino_areacomun').addClass("active");
+            configurarAutoCompleteAreaComun({onResponse: onResponseSearchAreacomun, onSelect: onSelectAreaComun});
+            configurarFechaSolicitud();
+        });
 
-function configurarAutoCompleteAreaComun(){
-    $('#areacomunautocomplete').autocomplete({
-        source: function (request, response) {
-            $.ajax("/api/areacomun/buscar?palabra="+request.term, {
-                global: false,
-                success: function (areaComunResponse) {
-                    response($.map(areaComunResponse.areasComunes, function (elem, idx) {
-                        return {
-                            label: elem.nombre,
-                            name: elem.nombre,
-                            code: elem.codigo
+        function configurarAutoCompleteAreaComun(if) {
+            $('#areacomunautocomplete').autocomplete({
+                source: function (request, response) {
+                    $.ajax("/api/areacomun/buscar?palabra=" + request.term, {
+                        global: false,
+                        success: function (areaComunResponse) {
+                            response($.map(areaComunResponse.areasComunes, function (elem, idx) {
+                                return {
+                                    label: elem.nombre,
+                                    name: elem.nombre,
+                                    code: elem.codigo
+                                }
+                            }));
                         }
-                    }));
-                }
+                    });
+                },
+                response: config.onResponse,
+                select: config.onSelect
             });
-        },
-        response: function (event, ui) {
+        }
+
+        function onResponseSearchAreacomun(e, ui) {
             if (ui.content.length <= 0) {
                 ui.content.push({label: "No se encontraron resultados"});
             }
-        },
-        select: function (event, ui) {
+        }
+
+        function onSelectAreaComun(e, ui) {
             $("#form_areaComun_codigo").val(ui.item.code);
         }
-    });
-}
 
-
-function configurarFechaSolicitud(){
-    $("#fechaSolicitud").change(function(){
-        $("#verificarDisponibilidad").click();
-    });
-}
-
-
-function escucharDisponibilidad(){
-    $('#fechaSolicitud').on('disponibilidad:trigger', function(event, disponibilidad){
-
-        if(disponibilidad.disponible){
-            $('.areacomun-disponibilidad .label-success').show();
-            $('.areacomun-disponibilidad .label-warning').hide();
-        }else {
-            $('.areacomun-disponibilidad .label-success').hide();
-            $('.areacomun-disponibilidad .label-warning').show();
+        function configurarFechaSolicitud() {
+            $("#fechaSolicitud").change(function () {
+                $("#verificarDisponibilidad").click();
+            });
         }
-    });
-}
+
+        function escucharDisponibilidad() {
+            $('#fechaSolicitud').on('disponibilidad:trigger', function (event, disponibilidad) {
+
+                if (disponibilidad.disponible) {
+                    $('.areacomun-disponibilidad .label-success').show();
+                    $('.areacomun-disponibilidad .label-warning').hide();
+                } else {
+                    $('.areacomun-disponibilidad .label-success').hide();
+                    $('.areacomun-disponibilidad .label-warning').show();
+                }
+            });
+        }
+    }
+));
