537b70ad68c1bc1005b18fb4c1b3eadbf2a4eea2
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-04-02 20:25:32 -0400

    refactor entidades.

--- src/test/java/equipo/once/elizabeth/richard/wilson/domains/AreaComunTest.java
+++ src/test/java/equipo/once/elizabeth/richard/wilson/domains/AreaComunTest.java
@@ -5,13 +5,25 @@ import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.*;
+
 public class AreaComunTest {
 
   @Test
   public void areaComunCodigo(){
     AreaComun areaComun = AreaComun.crear("A-piscina-1","Piscina");
 
-    Assert.assertThat(areaComun.nombre, Is.is("Piscina"));
-    Assert.assertThat(areaComun.codigo, Is.is("A-piscina-1"));
+    Assert.assertThat(areaComun.nombre, is("Piscina"));
+    Assert.assertThat(areaComun.codigo, is("A-piscina-1"));
   }
+
+  @Test
+  public void areaConElMismoCodigoSonIgualesTest() {
+    AreaComun piscina = AreaComun.crear("A-piscina-1","Piscina");
+    AreaComun otraarea = AreaComun.crear("A-piscina-1","Piscina");
+
+    Assert.assertThat(piscina, is(otraarea));
+
+  }
+
 }
--- src/test/java/equipo/once/elizabeth/richard/wilson/repositories/AreaComunRespositoryTest.java
+++ src/test/java/equipo/once/elizabeth/richard/wilson/repositories/AreaComunRespositoryTest.java
@@ -2,6 +2,8 @@ package equipo.once.elizabeth.richard.wilson.repositories;
 
 import equipo.once.elizabeth.richard.wilson.entities.dao.AreaComunRepositoryCustom;
 import equipo.once.elizabeth.richard.wilson.entities.dominio.AreaComun;
+import org.hamcrest.core.IsNot;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestData
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.*;
+import static org.mockito.internal.matchers.NotNull.NOT_NULL;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -25,6 +34,18 @@ public class AreaComunRespositoryTest{
         AreaComun areaComun = AreaComun.crear("A-piscina-01", "Piscina");
 
         repository.save(areaComun);
+    }
+
+    @Test
+    public void puedeBuscarTodasLasAreascomunesTest() {
+        Iterable<AreaComun> all = repository.findAll();
+
+        List<AreaComun> areaComuns = new ArrayList<>();
+        all.forEach(a -> areaComuns.add(a));
+
+
+        Assert.assertThat(all, is(NOT_NULL));
+        Assert.assertThat(areaComuns.size(), not(0));
     }
 
 }
