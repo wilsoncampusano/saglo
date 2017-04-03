dae2298ae3da463e877e3030fc471d85978ac88e
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-04-02 20:36:02 -0400

    data inicializacion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,6 @@ values
 ('Piscina', 'A-piscina-01'),
 ('Area BBQ', 'A-bbq-01'),
 ('Cancha Basketball', 'A-canchabaskt-01');
-
-
 insert into inquilino(codigo , nombres, apellidos)
 VALUES
 ('I-wilson-01', 'Wilson', 'Campusano Jorge'),
