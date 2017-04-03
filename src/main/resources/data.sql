c87358f0826755f270047a8c02dc17d522f53762
Wilson Campusano <wicampusano@arshumano.com>
2017-04-03 09:28:16 -0400

    fixing build

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,3 @@ values
 ('Piscina', 'A-piscina-01'),
 ('Area BBQ', 'A-bbq-01'),
 ('Cancha Basketball', 'A-canchabaskt-01');
-insert into inquilino(codigo,nombres, apellidos)
-VALUES
-('I-wilson-01', 'Wilson', 'Campusano Jorge'),
-('I-elizabeth-01','Elizabeth', 'Capellan ')
