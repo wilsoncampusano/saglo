9d8a81ba36fafe37c2e8de99016468ba01216aa2
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-06-04 14:26:54 -0400

    layout admin y tecnico

---
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/admin/AdminSolicitudesController.java
@@ -0,0 +1,7 @@
+package equipo.once.elizabeth.richard.wilson.controllers.view.admin;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminSolicitudesController extends AdminController {
+}
