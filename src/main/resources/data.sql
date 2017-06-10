6c28e1f8425c7ba099bb0ac4b538f743df2c03aa
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2017-06-09 20:42:04 -0400

    liquibase

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@ values
 ('Piscina', 'A-piscina-01'),
 ('Area BBQ', 'A-bbq-01'),
 ('Cancha Basketball', 'A-canchabaskt-01');
+
+
+insert into inquilino(nombre)
+    values
+('Elizabeth Capellan'),
+('Richard Jimenez');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,19 @@
+drop table if EXISTS  areacomun;
 
 CREATE TABLE areacomun (
-  id INTEGER IDENTITY PRIMARY KEY,
-  codigo VARCHAR(30),
-  nombre  VARCHAR(50)
-);
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(30) DEFAULT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+drop table IF EXISTS inquilino;
+
+create table inquilino (
+  id int(11) unsigned not null auto_increment,
+  codigo varchar(30) default 'cod',
+  nombre varchar(50) default '',
+  PRIMARY KEY (id)
+
+) engine=InnoDB default charset = latin1;
