90f6997f5f57824ac6a4b4c4d055d74878142316
Wilson Campusano <wicampusano@arshumano.com>
2018-04-11 13:38:55 -0400

    redirect

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/CommonViewController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/CommonViewController.java
@@ -1,11 +1,18 @@
 package equipo.once.elizabeth.richard.wilson.controllers.view;
 
+import equipo.once.elizabeth.richard.wilson.controllers.view.admin.AdminController;
+import equipo.once.elizabeth.richard.wilson.controllers.view.inquilino.InquilinoController;
+import equipo.once.elizabeth.richard.wilson.controllers.view.tecnico.TecnicoController;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 
 
@@ -25,4 +32,33 @@ public abstract class CommonViewController {
         webDataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
     }
 
+
+    public String obtenerRole(Authentication authentication){
+        Collection<GrantedAuthority> authorities = ((User) authentication.getPrincipal()).getAuthorities();
+        GrantedAuthority grantedAuthority = authorities.stream().findFirst().get();
+
+        String authority = grantedAuthority.getAuthority();
+        return authority;
+    }
+
+    public String paginaParaRole(String role){
+        String pagina = "";
+        switch (role){
+            case "ROLE_ADMIN" :
+                pagina = AdminController.ADMIN_INDEX_VIEW;
+                break;
+            case "ROLE_TECNICO" :
+                pagina = TecnicoController.TECNICO_INDEX_VIEW;
+
+                break;
+            case "ROLE_INQUILINO" :
+                pagina = InquilinoController.INQUILINO_INDEX;
+
+                break;
+            case "ROLE_PROPIETARIO" :
+                pagina = AdminController.ADMIN_INDEX_VIEW;
+                break;
+        }
+       return pagina;
+    }
 }
--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/UsuarioController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/UsuarioController.java
@@ -6,17 +6,22 @@ import equipo.once.elizabeth.richard.wilson.services.SecurityService;
 import equipo.once.elizabeth.richard.wilson.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Collection;
+import java.util.Optional;
+
 /**
  * Created on 4/11/2018.
  */
 @Controller
-public class UsuarioController {
+public class UsuarioController extends CommonViewController{
     @Autowired
     private UsuarioService usuarioService;
 
@@ -61,7 +66,9 @@ public class UsuarioController {
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model, Authentication authentication) {
-        return "welcome";
+        String ROLE = obtenerRole(authentication);
+        String paginaParaRole = paginaParaRole(ROLE);
+        return "redirect:"+paginaParaRole;
     }
 
 }
