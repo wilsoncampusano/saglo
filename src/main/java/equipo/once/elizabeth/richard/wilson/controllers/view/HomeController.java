a320fc0ad4f333eb25b0a349c8a02b5ac5a76323
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-04-11 19:46:54 -0400

    acerca de

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/HomeController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/HomeController.java
@@ -2,7 +2,16 @@ package equipo.once.elizabeth.richard.wilson.controllers.view;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+  @RequestMapping(value = "/acerca", method = RequestMethod.GET)
+  public String registration(Model model) {
+
+    return "acerca";
+  }
 }
