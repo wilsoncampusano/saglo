35b435931f043558b955c47dc6ec425a32fe75b8
Wilson Campusano <wicampusano@arshumano.com>
2018-04-12 10:26:02 -0400

    test

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/admin/AdminController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/admin/AdminController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController extends CommonViewController {
-  public static final String ADMIN_INDEX_VIEW = "/admin/index";
+  public static final String ADMIN_INDEX_VIEW = "admin/index";
 
 }
