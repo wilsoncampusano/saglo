72b3f5d2d7a766f896211534bad666abcb0abf91
Wilson Campusano <wicampusano@arshumano.com>
2018-04-12 09:22:56 -0400

    log

--- src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/admin/IndexAdminController.java
+++ src/main/java/equipo/once/elizabeth/richard/wilson/controllers/view/admin/IndexAdminController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Objects;
+
 
 @Controller
 public class IndexAdminController extends AdminController {
@@ -13,7 +15,11 @@ public class IndexAdminController extends AdminController {
   @RequestMapping(value = {"/","","/index", "home"}, method = RequestMethod.GET)
   public ModelAndView adminIndexGet(Authentication authentication){
     ModelAndView modelAndView = new ModelAndView(ADMIN_INDEX_VIEW);
-    logger.warning(String.format("USUARIO EN SESION : %s con %s , %s", ADMIN_INDEX_VIEW, authentication.getPrincipal(), authentication.getDetails()));
+    if(Objects.nonNull(authentication)){
+      logger.warning(String.format("USUARIO EN SESION : %s con %s , %s", ADMIN_INDEX_VIEW, authentication.getPrincipal(), authentication.getDetails()));
+    }else{
+      logger.warning(String.format("NO USUARIO EN SESION %s", ADMIN_INDEX_VIEW));
+    }
 
     return modelAndView;
   }
